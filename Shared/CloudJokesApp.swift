43a427cddd92ebaebb8bcb060d7fb0f299bdeceb
Pranav Kasetti <pranav.kasetti@kinandcarta.com>
2022-02-23 17:32:31 +0000

    Initial Commit

---
+++ Shared/CloudJokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudJokesApp.swift
+//  Shared
+//
+//  Created by Pranav Kasetti on 23/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CloudJokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
