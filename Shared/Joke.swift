6d9dd630bec7bc2c784e890008799f2fa2f770af
Pranav Kasetti <pranav.kasetti@kinandcarta.com>
2022-02-24 12:06:01 +0000

    Add all jokes and imageURL support

--- Shared/Joke.swift
+++ Shared/Joke.swift
@@ -8,20 +8,32 @@
 import Foundation
 
 struct Joke {
-
-  static let allJokes = [Joke(question: "Where do clouds go to the bathroom?",
-                              answer: "Anywhere they want!"),
-                         Joke(question: "What do you get if you mix a very sad cloud and the ocean?",
-                              answer: "A tropical depression!"),
-                         Joke(question: "Why was the sun mad at the clouds?",
-                              answer: "Because they kept throwing shade!"),
-                         Joke(question: "I thought I saw a cloud that looked like a pastry.",
-                              answer: "But it was a pie in the sky!"),
-                         Joke(question: "Why was the cloud so dark and stormy?",
-                              answer: "It was mis-thunder-stood!"),
-  ]
-
   var question: String
   var answer: String
+  private var imagePath: String
+  var imageURL: URL? {
+    let comps = imagePath.components(separatedBy: ".")
+    let name = comps[0]
+    let fileType = comps[1]
+    return Bundle.main.url(forResource: name, withExtension: fileType)
+  }
 }
 
+extension Joke {
+  static let allJokes = [Joke(question: "Where do clouds go to the bathroom?",
+                              answer: "Anywhere they want!",
+                              imagePath: "jokeone.jpeg"),
+                         Joke(question: "What do you get if you mix a very sad cloud and the ocean?",
+                              answer: "A tropical depression!",
+                              imagePath: "joketwo.jpeg"),
+                         Joke(question: "Why was the sun mad at the clouds?",
+                              answer: "Because they kept throwing shade!",
+                              imagePath: "jokethree.jpeg"),
+                         Joke(question: "I thought I saw a cloud that looked like a pastry.",
+                              answer: "But it was a pie in the sky!",
+                              imagePath: "jokefour.jpeg"),
+                         Joke(question: "Why was the cloud so dark and stormy?",
+                              answer: "It was mis-thunder-stood!",
+                              imagePath: "jokefive.jpeg"),
+  ]
+}
