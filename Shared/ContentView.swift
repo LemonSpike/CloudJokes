d0af874c044595eed1def03ccbfa83ec25489c92
Pranav Kasetti <pranav.kasetti@kinandcarta.com>
2022-02-25 10:49:31 +0000

    Hook up ContentView to ViewModel with basic styling

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+
+  @StateObject var viewModel = JokeViewModel()
+
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    NavigationView {
+      VStack {
+        Button("Joke") {
+          viewModel.nextJoke()
+        }.font(.headline)
+          .padding()
+          .buttonStyle(.bordered)
+        Text(viewModel.jokeQuestion)
+          .font(.title)
+          .padding(20)
+          .multilineTextAlignment(.center)
+        Spacer()
+        AsyncImage(url: viewModel.jokeImageURL)
+        Spacer()
+        Text(viewModel.jokeAnswer)
+          .font(.title2)
+          .padding(20)
+          .multilineTextAlignment(.center)
+        Spacer()
+      }
+      .navigationTitle("Cloud Jokes")
+    }
   }
 }
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
+      .environment(\.colorScheme, .light)
+      .previewDevice(PreviewDevice(rawValue: "iPhone 12 Pro"))
   }
 }
